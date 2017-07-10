b6ed2ff59c032227f48abe4b60de4e9b257c96dc
Alex Hoff <hoffx231@umn.edu>
2017-07-09 20:52:14 -0700

    Adding the time force page.

--- app/src/main/java/com/example/owner/autowakerapp/TimeForcer.java
+++ app/src/main/java/com/example/owner/autowakerapp/TimeForcer.java
@@ -1,10 +1,26 @@
 package com.example.owner.autowakerapp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by Owner on 7/9/2017.
  */
 
 public class TimeForcer extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("debug", "Testing.");
+        setContentView(R.layout.time_forcer);
+    }
+
+    public void onButtonClick(View view) {
+        if (view.getId() == R.id.homeButton){
+            Intent i = new Intent(this, SleepSetup.class);
+            startActivity(i);
+        }
+    }
 }
