3cc495ed8452245e4836ef9004fad9ba478ed490
Alex Hoff <hoffx231@umn.edu>
2017-07-10 21:06:31 -0700

    Adding some functionality, limited the minimum number of hours to 5<=hrs<=12.

--- app/src/main/java/com/example/owner/autowakerapp/MinHours.java
+++ app/src/main/java/com/example/owner/autowakerapp/MinHours.java
@@ -6,4 +6,18 @@ package com.example.owner.autowakerapp;
 
 public class MinHours {
 
+    private int minHours = 5;
+
+    public int getMinHours() {
+        return 5;
+    }
+
+    public void setMinHours( int minHours ) {
+        if (minHours >= 12) {
+            // TODO: Pass message back up
+        } else if (minHours >= 5){
+            this.minHours = minHours;
+        }
+
+    }
 }
