d304b9f1765bb98af475f3abf313a43da74902f5
Alex Hoff <hoffx231@umn.edu>
2017-07-10 21:46:26 -0700

    Adding a home button for the minimum hour page.

--- app/src/main/java/com/example/owner/autowakerapp/MinHours.java
+++ app/src/main/java/com/example/owner/autowakerapp/MinHours.java
@@ -1,14 +1,32 @@
 package com.example.owner.autowakerapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 /**
  * Created by Owner on 7/9/2017.
  */
 
-public class MinHours {
+public class MinHours extends Activity{
 
     private double minHours = 5;
     private int maximumMinHours = 12;
     private int minimumMinHours = 5;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.min_hours);
+    }
+
+    public void onButtonClick(View view) {
+        if (view.getId() == R.id.homeButton){
+            Intent i = new Intent(this, SleepSetup.class);
+            startActivity(i);
+        }
+    }
 
     public double getMinHours() {
         return minHours;
