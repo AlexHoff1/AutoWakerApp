a504fc86e53094bb879d338714ec6e4aee99c928
Alex Hoff <hoffx231@umn.edu>
2017-07-10 21:57:34 -0700

    Adding a http link idea to the backendlink file. This is so we can actually retrieve when the user is supposed to wake up. Currently this will also need to construct the URL based on the username, so we'll need to have another way to retrieve that (it can't be hardcoded).

--- app/src/main/java/com/example/owner/autowakerapp/BackendLink.java
+++ app/src/main/java/com/example/owner/autowakerapp/BackendLink.java
@@ -1,14 +1,36 @@
 package com.example.owner.autowakerapp;
 
+import android.util.Log;
+
+import java.io.*;
+import java.net.*;
+
 /**
  * Created by Owner on 7/9/2017.
  */
 
 public class BackendLink {
+
+    private static final String WEBSITE_URL = "www.yahoo.com";
     public void send(){
 
     }
     public void getWakeTime(){
+        try {
+            URL base = new URL(WEBSITE_URL);
+            URLConnection opened_link = base.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(opened_link.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null){
+                Log.d("Sleep", "Information was retrieved.");
+                // TODO: Add this wake time to a config file somewhere.
+            }
+            in.close();
 
+        } catch (MalformedURLException e) {
+            Log.d("Sleep", "Not sure why, but the URL is invlaid. Better check that out!");
+        } catch (IOException e) {
+            Log.d("Sleep", "Not sure why, but there was an IOException.");
+        }
     }
 }
