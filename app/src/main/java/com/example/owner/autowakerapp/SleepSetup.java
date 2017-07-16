f0a0ae3d116d394ecfde4b24bf07a09e6584d86b
Alex Hoff <hoffx231@umn.edu>
2017-07-15 21:47:03 -0700

    Added music on button click and calling backend service.

--- app/src/main/java/com/example/owner/autowakerapp/SleepSetup.java
+++ app/src/main/java/com/example/owner/autowakerapp/SleepSetup.java
@@ -2,6 +2,8 @@ package com.example.owner.autowakerapp;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,8 +18,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SleepSetup extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private AsyncTask<URL, Integer, String> timeGetter;
+    private MediaPlayer pyromania;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,24 +53,59 @@ public class SleepSetup extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        try {
+            timeGetter = new BackendLink("wakeTime");
+            timeGetter.execute(new URL(retrieveFullURL()));
+        } catch (Exception e) {
+            Log.e("URL", "Some weird error happened.");
+        }
+        BackgroundRunner testing = new BackgroundRunner();
+        this.pyromania = MediaPlayer.create(this, R.raw.daycore_pyromania);
     }
 
     public void onButtonClick(View view) {
         int id = view.getId();
-        if (id>0) {
+        try {
+            this.pyromania.stop();
+            this.pyromania = MediaPlayer.create(this, R.raw.daycore_pyromania);
+            if ( this.pyromania.getCurrentPosition() == 0 ) {
+                this.pyromania.start();
+            }
+        } catch (Exception e) {
+            Log.d("bug", "bug on click: " + e.toString());
+        }
+        if (id!=-1) {
             Intent i = new Intent(this, SleepSetup.class);
             if (id == R.id.syncButton) {
                 i = new Intent(this, BlutoothConnection.class);
             } else if (id == R.id.dateButton) {
                 i = new Intent(this, DaysToWakeUp.class);
             } else if (id == R.id.homeButton) {
-                i = new Intent(this, SleepSetup.class);
+                try {
+                    Log.d("Time", "wellll: " + timeGetter.get());
+                } catch (java.util.concurrent.ExecutionException e) {
+                    Log.e("Error", "thread didn't execute " + e.toString());
+                } catch (Exception e) {
+                    Log.e("Error", "Unknown exception " + e.toString());
+                }
+                // Make this run in background.
+                BackgroundRunner b = new BackgroundRunner("wakeTime");
+                i = new Intent(this, BackgroundRunner.class);
+                try {
+                    i.setData(Uri.parse("http://192.168.56.1:8888/?date=2017/07/15&user=5T23R6"));
+                } catch (Exception e) {
+                    Log.e("background", "Starting background failed." + e.toString());
+                }
+                this.startService(i);
+                return;
             } else if (id == R.id.timeButton) {
                 i = new Intent(this, TimeForcer.class);
             } else if (id == R.id.minHoursButton) {
                 i = new Intent(this, MinHours.class);
             }
-            startActivity(i);
+            startActivityForResult(i, 0);
+        } else {
+            Log.d("Interesting", "ID was actually negative >.>");
         }
     }
 
@@ -120,4 +165,29 @@ public class SleepSetup extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private static final String WEBSITE_URL = "http://192.168.56.1:8888/";
+
+    private String retrieveFullURL() {
+        // TODO: Assemble appropriate link and parameters as a URL object, not string ;L
+        String date = getCurrentDate();
+        String current_user = getCurrentUser();
+        //Path filePath = Paths.get(WEBSITE_URL, "sleep", current_user, date);
+        //return filePath.toString();
+        return WEBSITE_URL + "?date=" + date + "&user=" + current_user;
+    }
+
+    private String getCurrentDate() {
+        // TODO: Clean this up
+        DateFormat date_format = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        String date_string = date_format.format(date);
+        return date_string;
+    }
+
+    private String getCurrentUser() {
+        // TODO: Extract user from some config
+        return "5T23R6";
+    }
+
 }
