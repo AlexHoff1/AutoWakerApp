c7091c9265d75054a0bfbf0650893f4b9766e4b5
Alex Hoff <hoffx231@umn.edu>
2017-07-16 21:32:30 -0700

    Making a helper runnable to try and get the data without crashing the application.

--- app/src/main/java/com/example/owner/autowakerapp/WorkerThread.java
+++ app/src/main/java/com/example/owner/autowakerapp/WorkerThread.java
@@ -1,6 +1,9 @@
 package com.example.owner.autowakerapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.util.Log;
 
 /**
  * Created by Owner on 7/15/2017.
@@ -9,10 +12,9 @@ import android.content.Intent;
 
 public class WorkerThread implements Runnable {
 
-    @Override
     public void run() {
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
-
+        Log.i("Thread", "In thread.");
         Intent i = new Intent();
 
         BackgroundRunner testing = new BackgroundRunner();
