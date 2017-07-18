dda4c89d0dbf5aaee7558b6c99078435956f0a5c
Alex Hoff <hoffx231@umn.edu>
2017-07-17 21:33:05 -0700

    Making the variable static so that each instance can turn off or on existing music.

--- app/src/main/java/com/example/owner/autowakerapp/NoiseMaker.java
+++ app/src/main/java/com/example/owner/autowakerapp/NoiseMaker.java
@@ -12,14 +12,14 @@ public class NoiseMaker implements Runnable {
 
     private static MediaPlayer pyromania;
     private SleepSetup sleep = null;
-    private boolean playing = false;
+    private static boolean playing = false;
     public NoiseMaker(SleepSetup a){
         this.sleep = a;
     }
     @Override
     public void run() {
-        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
-        if (pyromania!=null || this.playing) {
+        //android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
+        if (this.playing) {
             try {
                 this.pyromania.stop();
                 this.playing = false;
